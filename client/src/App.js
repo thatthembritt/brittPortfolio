2960214189fdeb0ca337651adb589258bd7f1573
Brittany Boyd <brttnybyd@gmail.com>
2023-01-27 13:00:15 -0700

    updated routes and contact component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route path="/Home" element={<Search />} />
+        <Route path="/Contact" element={<Search />} />
+        <Route path="/Footer" element={<Search />} />
+        <Route path="/Navbar" element={<Search />} />
+        <Route path="/Projects" element={<Search />} />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+
+const ContactMe = function () {
+  return (
+    <div>
+      <Navbar />
+      <Contact />
+    </div>
+  );
+};
+
+export default Contact;
