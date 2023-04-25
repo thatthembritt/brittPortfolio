95f16f4640459538c9b8bffb54b0305b51b39b82
Brittany Boyd <brttnybyd@gmail.com>
2023-04-25 12:33:41 -0600

    updated resume page

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-#React Portfolio
+# React Portfolio
 
-##Description
+## Description
 
 This is my personal website and portfolio. This is a React application and features myu favorite color: yellow!
 
-##Usage
+## Usage
 
 Please visit the below link to see my live website, enjoy!
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -35,106 +35,110 @@ const Resume = () => {
           DOWNLOAD RESUME
         </Button>
       </Grid>
-      <Grid item>
-        <Card
-          style={{
-            backgroundColor: "#fcc931",
-            fontFamily: "Odibee Sans",
-            color: "#3f51b5",
-            textDecoration: "none",
-            cursor: "pointer",
-            backgroundColor: "#fcc931",
-            color: "black",
-            size: "small",
-            alignItems: "center",
-            margin: 2,
-          }}
-        >
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Extracurricular Interests
-            </Typography>
-            <Typography variant="body1">
-              <ul>
-                <li>Standup comedy</li>
-                <li>Piano</li>
-                <li>Comic books</li>
-                <li>Weightlifting</li>
-              </ul>
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card
-          style={{
-            backgroundColor: "#fcc931",
-            fontFamily: "Odibee Sans",
-            color: "#3f51b5",
-            textDecoration: "none",
-            cursor: "pointer",
-            backgroundColor: "#fcc931",
-            color: "black",
-            size: "small",
-            alignItems: "center",
-            margin: 2,
-          }}
-        >
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Education
-            </Typography>
-            <Typography variant="body1">
-              <ul>
-                <li>
-                  2023
+      <Grid item container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card
+            style={{
+              backgroundColor: "#fcc931",
+              color: "#3f51b5",
+              textDecoration: "none",
+              cursor: "pointer",
+              backgroundColor: "#fcc931",
+              color: "black",
+              size: "small",
+              alignItems: "center",
+              margin: 2,
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5" component="h2" fontFamily="Odibee Sans">
+                Extracurricular Interests
+              </Typography>
+              <Typography variant="body1" fontFamily="Odibee Sans">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Standup comedy</li>
+                  <li>Piano</li>
+                  <li>Comic books</li>
+                  <li>Weightlifting</li>
+                </ul>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card
+            style={{
+              backgroundColor: "#fcc931",
+              color: "#3f51b5",
+              textDecoration: "none",
+              cursor: "pointer",
+              backgroundColor: "#fcc931",
+              color: "black",
+              size: "small",
+              alignItems: "center",
+              margin: 2,
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5" component="h2" fontFamily="Odibee Sans">
+                Education
+              </Typography>
+              <Typography variant="body1" fontFamily="Odibee Sans">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>
+                    2023
+                    <br />
+                    University of Denver
+                    <br />A full time intensive program focused on gaining
+                    technical programming skills in HTML5, CSS3, Javascript,
+                    JQuery, Bootstrap, Node.js, MySQL, MongoDB, Express,
+                    Handelbars.js, and ReactJS.
+                  </li>
                   <br />
-                  University of Denver
-                  <br />A full time intensive program focused on gaining
-                  technical programming skills in HTML5, CSS3, Javascript,
-                  JQuery, Bootstrap, Node.js, MySQL, MongoDB, Express,
-                  Handelbars.js, and ReactJS.
-                </li>
-                <li>
-                  2014
-                  <br />
-                  Eastern Kentucky University
-                  <br />
-                  Bachelor of Art's in Broadcasting & Film Technology
-                </li>
-              </ul>
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card
-          style={{
-            backgroundColor: "#fcc931",
-            fontFamily: "Odibee Sans",
-            color: "#3f51b5",
-            textDecoration: "none",
-            cursor: "pointer",
-            backgroundColor: "#fcc931",
-            color: "black",
-            size: "small",
-            alignItems: "center",
-            margin: 2,
-          }}
-        >
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Achievements
-            </Typography>
-            <Typography variant="body1">
-              <ul>
-                <li>
-                  2022
-                  <br />
-                  Altercation Comedy Festival
-                </li>
-                <li>CloudTop Comedy Festival</li>
-                <li>Semi-Finalist in Fort's Funniest Comedy Competition</li>
-              </ul>
-            </Typography>
-          </CardContent>
-        </Card>
+                  <li>
+                    2014
+                    <br />
+                    Eastern Kentucky University
+                    <br />
+                    Bachelor of Art's in Broadcasting & Film Technology
+                  </li>
+                </ul>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card
+            style={{
+              backgroundColor: "#fcc931",
+              color: "#3f51b5",
+              textDecoration: "none",
+              cursor: "pointer",
+              backgroundColor: "#fcc931",
+              color: "black",
+              size: "small",
+              alignItems: "center",
+              margin: 2,
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5" component="h2" fontFamily="Odibee Sans">
+                Achievements
+              </Typography>
+              <Typography variant="body1" fontFamily="Odibee Sans">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>
+                    2022
+                    <br />
+                    Altercation Comedy Festival
+                  </li>
+                  <li>CloudTop Comedy Festival</li>
+                  <li>Semi-Finalist in Fort's Funniest Comedy Competition</li>
+                </ul>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </Grid>
   );
